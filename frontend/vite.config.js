253a5846aa1bfaa8ef7d8a25a4bc8e9448701eaa
Sumit03021 <sumitkumar03021997@gmail.com>
2024-06-23 18:54:38 +0530

    updated vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
       '/socket.io': {
         target: 'http://localhost:8080',
         changeOrigin: true,
